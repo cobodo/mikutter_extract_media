2d0e791f4d5f564e0fdb8b97252a961a6ba90e8d
cobodo <cobodo@gmail.com>
2018-02-06 19:19:07 +0900

    photo filterを利用して画像っぽいものをすべて抜き出すように変更

--- mikutter_extract_media.rb
+++ mikutter_extract_media.rb
@@ -1,5 +1,11 @@
 Plugin.create :extract_media do
   defextractcondition(:include_media, name: _('メディアを含む'), operator: false, args: 0) do |message:raise|
-    message.entity.any?{ |entity| entity[:slug] == :media }
+    message.entity.any?{|entity|
+      Enumerator.new{|y|
+        Plugin.filtering(:openimg_image_openers, y)
+      }.any?{|opener|
+        opener.condition === entity[:open]
+      }
+    }
   end
 end
