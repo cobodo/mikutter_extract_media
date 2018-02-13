775f26fd24e665110aa7f53d035bf1fefa401e12
cobodo <cobodo@gmail.com>
2018-02-13 15:58:22 +0900

    entity[:open]の認識が甘かったので修正

--- mikutter_extract_media.rb
+++ mikutter_extract_media.rb
@@ -1,10 +1,21 @@
 Plugin.create :extract_media do
   defextractcondition(:include_media, name: _('メディアを含む'), operator: false, args: 0) do |message:raise|
     message.entity.any?{|entity|
+      if entity[:open].is_a?(Plugin::Photo::Photo)
+        next true
+      end
+      uri = nil
+      if entity[:open].is_a?(String)
+        uri = entity[:open]
+      elsif entity[:open].is_a?(Diva::Model)
+        uri = entity[:open].uri
+      end
+      next false if uri.nil?
+
       Enumerator.new{|y|
         Plugin.filtering(:openimg_image_openers, y)
       }.any?{|opener|
-        opener.condition === entity[:open]
+        opener.condition === uri
       }
     }
   end
