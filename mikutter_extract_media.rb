b04011f8c01161ba45a37cca6988164b8ce3241d
cobodo <cobodo@gmail.com>
2018-05-02 21:53:07 +0900

    score対応

--- mikutter_extract_media.rb
+++ mikutter_extract_media.rb
@@ -1,21 +1,12 @@
 Plugin.create :extract_media do
   defextractcondition(:include_media, name: _('メディアを含む'), operator: false, args: 0) do |message:raise|
-    message.entity.any?{|entity|
-      if entity[:open].is_a?(Plugin::Photo::Photo)
-        next true
-      end
-      uri = nil
-      if entity[:open].is_a?(String)
-        uri = entity[:open]
-      elsif entity[:open].is_a?(Diva::Model)
-        uri = entity[:open].uri
-      end
-      next false if uri.nil?
+    uris = score_of(message).map(&:uri).map(&:to_s)
 
-      Enumerator.new{|y|
-        Plugin.filtering(:openimg_image_openers, y)
-      }.any?{|opener|
-        opener.condition === uri.to_s
+    Enumerator.new{|y|
+      Plugin.filtering(:openimg_image_openers, y)
+    }.any?{|opener|
+      uris.any?{|uri|
+        opener.condition === uri
       }
     }
   end
