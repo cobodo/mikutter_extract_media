4c4ac6e5f7f6cc9577e5b247380be620469703f6
cobodo <cobodo@gmail.com>
2018-05-07 23:52:19 +0900

    通常の画像URLが抜けていたので修正

--- mikutter_extract_media.rb
+++ mikutter_extract_media.rb
@@ -1,6 +1,12 @@
 Plugin.create :extract_media do
+  exts = Gtk::FormDSL::PIXBUF_PHOTO_FILTER.values.flatten.map(&:downcase).uniq.map {|ext| ".#{ext}" }
+
   defextractcondition(:include_media, name: _('メディアを含む'), operator: false, args: 0) do |message:raise|
     uris = score_of(message).map(&:uri).map(&:to_s)
+    has_normal_image = uris.any? do |uri|
+      exts.any? {|ext| uri.downcase.end_with? ext}
+    end
+    next true if has_normal_image
 
     Enumerator.new{|y|
       Plugin.filtering(:openimg_image_openers, y)
