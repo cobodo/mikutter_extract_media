71a7ab82f43e0574369486673ce8bfdbe492969f
cobodo <cobodo@gmail.com>
2018-05-12 23:10:11 +0900

    カスタム絵文字画像を検索対象から外した

--- mikutter_extract_media.rb
+++ mikutter_extract_media.rb
@@ -2,7 +2,7 @@ Plugin.create :mikutter_extract_media do
   exts = Gtk::FormDSL::PIXBUF_PHOTO_FILTER.values.flatten.map(&:downcase).uniq.map {|ext| ".#{ext}" }
 
   defextractcondition(:include_media, name: _('メディアを含む'), operator: false, args: 0) do |message:raise|
-    uris = score_of(message).map(&:uri).map(&:to_s)
+    uris = score_of(message).select{|note| !note.respond_to?(:inline_photo) }.map(&:uri).map(&:to_s)
     has_normal_image = uris.any? do |uri|
       exts.any? {|ext| uri.downcase.end_with? ext}
     end
